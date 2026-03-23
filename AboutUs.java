85a36ce285e4c1bfca36b0f1a77ae094e0fa5f92
Redty-code <borapicheth63@gmail.com>
2026-03-23 10:28:25 +0700

    picheth

---
+++ AboutUs.java
@@ -0,0 +1,12 @@
+//Task 7
+public class AboutUs {
+    public static void main(String[] args) {
+
+        System.out.println("--- About Us ---");
+        System.out.println("I3 Typing Master v2026");
+        System.out.println("Developed by:");
+        System.out.println("- Redty-code");
+        System.out.println("- aovcheasin");
+        System.out.println("- HeisenbergPC");
+    }
+}
---
+++ TestResults.java
@@ -0,0 +1,14 @@
+//Task 5
+public class TestResults {
+    public static void main(String[] args) {
+
+        String username = "admin";
+        int speed = 65;
+        int accuracy = 92;
+
+        System.out.println("--- Test Results ---");
+        System.out.println("Username: " + username);
+        System.out.println("Typing Speed: " + speed + " WPM");
+        System.out.println("Accuracy: " + accuracy + "%");
+    }
+}
